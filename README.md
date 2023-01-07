372a4ea88b06138e3c4de56eec64f6b2c323ec16
Joel Azevedo <joeellazz@gmail.com>
2023-01-06 23:14:46 -0300

    App-clima

---
+++ README.md
@@ -0,0 +1,4 @@
+# Consultas de clima cidades
+### Aplicação em javascript ultizando OpenWeatherApi 
+
+projeto simples para consilidar os estudos em async await , pode-se consultar clima de cidades estará disponivel a temperatura, condicão climatica, umidade e vento.
---
+++ js/script.js
@@ -0,0 +1,61 @@
+const apiKey = 'd15b869830b389d5df5cfb4fbda2ed9f';
+const apiCountryUrl = 'https://countryflagsapi.com/png/';
+
+const cityInput = document.querySelector('#city-input');
+const searchBtn = document.querySelector('#search');
+
+
+const cityElement = document.querySelector('#city');
+const tempElement = document.querySelector('#temperature span');
+const descElement = document.querySelector('#description');
+const weatherIconElement = document.querySelector('#weather-icon');
+const countryElement = document.querySelector('#country');
+const umidityElement = document.querySelector('#umidity span');
+const windElement = document.querySelector('#wind span');
+const weatherContainer = document.querySelector('#weather-data');
+
+// Funções
+const getWeatherData = async (city) => {
+
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+
+    const res = await fetch(apiWeatherURL);
+    if(res.status == 404){
+       alert("Digite uma cidade válida")
+       document.querySelector('#city-input').value = ''
+    }
+    const data = await res.json();
+    return data;
+
+}
+
+const showWeatherData = async (city) => {
+
+    const data = await getWeatherData(city);
+    cityElement.innerText = data.name;
+    tempElement.innerText = parseInt(data.main.temp);
+    descElement.innerText = data.weather[0].description;
+    weatherIconElement.setAttribute(
+        "src",
+        `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+    countryElement.setAttribute("src", apiCountryUrl + data.sys.country);
+    umidityElement.innerText = `${data.main.humidity}%`;
+    windElement.innerText = `${data.wind.speed}km/h`;
+
+    weatherContainer.classList.remove('hide');
+}
+
+// Eventos
+
+searchBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const city = cityInput.value;
+    showWeatherData(city);
+});
+
+cityInput.addEventListener("keyup", (e) => {
+    if (e.code == "Enter") {
+        const city = e.target.value;
+        showWeatherData(city);
+    }
+})
