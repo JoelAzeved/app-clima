99b87c3358d0adcc1c1cea911e5dde1d14b34561
JoelAzevedo <joeellazz@gmail.com>
2024-02-04 01:03:33 -0300

    add HTMLElement

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -1,13 +1,51 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
-var __webpack_exports__ = {};
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.ts":
 /*!**********************!*\
   !*** ./src/index.ts ***!
   \**********************/
+/***/ (function() {
 
-const dieka = 'dieka';
-console.log(dieka);
 
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const apiKey = 'd15b869830b389d5df5cfb4fbda2ed9f';
+const cityInput = document.querySelector('.input-city');
+const btn = document.querySelector('.btnSearch');
+const getDataWeather = (city) => __awaiter(void 0, void 0, void 0, function* () {
+    const apiWeather = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+    const res = yield fetch(apiWeather);
+    const data = yield res.json();
+    console.log(data);
+    return data;
+});
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const city = cityInput.value;
+    getDataWeather(city);
+});
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module is referenced by other modules so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.ts"]();
+/******/ 	
 /******/ })()
 ;
 //# sourceMappingURL=bundle.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,72 @@
-const dieka = 'dieka';
-console.log(dieka);
+const apiKey = 'd15b869830b389d5df5cfb4fbda2ed9f';
+type WeatherData = {
+  main: {
+    temp: number;
+    humidity: number;
+  };
+  weather: {
+    description: string;
+    icon: string;
+    name: string;
+  };
+  sys: {
+    country: string;
+  };
+};
+
+const cityInput = document.querySelector('.input-city') as HTMLInputElement;
+const btn = document.querySelector('.btnSearch') as HTMLButtonElement;
+
+const cityElement = document.querySelector('#city') as HTMLSpanElement;
+const tempElement = document.querySelector(
+  '#temperature span',
+) as HTMLParagraphElement;
+const descElement = document.querySelector(
+  '#description',
+) as HTMLParagraphElement;
+const weatherIconElement = document.querySelector(
+  '#weather-icon',
+) as HTMLParagraphElement;
+const countryElement = document.querySelector('#country') as HTMLSpanElement;
+const umidityElement = document.querySelector(
+  '#umidity span',
+) as HTMLSpanElement;
+const windElement = document.querySelector('#wind span') as HTMLSpanElement;
+
+const weatherContainer = document.querySelector(
+  '#weather-data',
+) as HTMLDivElement;
+
+const errorMessageContainer = document.querySelector(
+  '#error-message',
+) as HTMLDivElement;
+const loader = document.querySelector('#loader') as HTMLDivElement;
+
+const suggestionContainer = document.querySelector(
+  '#suggestions',
+) as HTMLDivElement;
+const suggestionButtons = document.querySelectorAll(
+  '#suggestions button',
+) as HTMLButtonElement;
+
+const getDataWeather = async (city: string): Promise<WeatherData> => {
+  const apiWeather = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+  try {
+    const res: Response = await fetch(apiWeather);
+    if (!res.ok) {
+      throw new Error(`Erro na requisição: ${res.statusText}`);
+    }
+    const data: WeatherData = await res.json();
+    console.log(data);
+    return data;
+  } catch (erroe) {
+    console.log(erroe);
+    throw erroe;
+  }
+};
+
+btn.addEventListener('click', (e: MouseEvent) => {
+  e.preventDefault();
+  const city = cityInput.value as string;
+  getDataWeather(city);
+});
